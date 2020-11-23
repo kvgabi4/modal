07f1759330bdc32100315aa4e69b4436999f943b
kvgabi4 <kvgabi4@gmail.com>
2020-11-23 07:45:28 +0100

    tab billentyű használata

--- js/modal.js
+++ js/modal.js
@@ -4,14 +4,15 @@ const hide = document.querySelectorAll('.hide');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
-function openModal(element) {
-    for (let i = 0; i < element.length; i += 1) {
-        element[i].classList.remove('hide');
+function openModal(event) {
+    for (let i = 0; i < event.length; i += 1) {
+        event[i].classList.remove('hide');
     }
 };
 
 function getFocus(event) {
     event.focus();
+    event.classList.add('modal-focus');
 }
 
 (function clickOpenModal() {
@@ -22,21 +23,21 @@ function getFocus(event) {
     })
 })();
 
-function closeModal(element) {
-    element.classList.add('hide');
+function closeModal(event) {
+    event.classList.add('hide');
 };
 
-function loseFocus(event) {
-    event.blur();
+function transition(event) {
+    event.classList.remove('modal-focus');
 }
 
 (function clickCloseModal() {
     const classes = document.querySelectorAll('.overlay, .okey, .cancel, .fa.fa-times');
     for (let i = 0; i < classes.length; i += 1) {
         classes[i].addEventListener('click', () => {
-            loseFocus(modal);
-            closeModal(overlay);
-            closeModal(modal);
+            transition(modal);
+            setTimeout(() => closeModal(overlay), 300);
+            setTimeout(() => closeModal(modal), 300);
         });
     }
 })();
